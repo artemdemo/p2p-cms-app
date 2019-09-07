cd84727f35396fbbf22a6002f965956c2638ff74
Artem Demo <artemdemo@gmail.com>
2019-09-07 08:02:25 +0300

    replaced server with express

--- server/server.js
+++ server/server.js
@@ -1,15 +1,9 @@
-const nodeStatic = require('node-static');
-const http = require('http');
-const file = new nodeStatic.Server(`${__dirname}/public`)
+const express = require('express');
 
-const server = http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response)
-    }).resume()
-}).listen(9990);
-
-server.on('error', function(err) {
-    if (err.code === 'EADDRINUSE') {
-        console.log('[server.js] EADDRINUSE');
-    }
-});
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(9990);
